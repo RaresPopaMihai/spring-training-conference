cd5952e7e6060d0321cfb8851a43f9dcc50cc01c
rares.popa@sap.com <rares.popa@sap.com>
2023-03-29 16:15:12 +0300

    xml config setter injection

--- src/main/java/com/tutorials/spring/Application.java
+++ src/main/java/com/tutorials/spring/Application.java
@@ -1,5 +1,8 @@
 package com.tutorials.spring;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 import com.tutorials.spring.services.SpeakerService;
 import com.tutorials.spring.services.SpeakerServiceImpl;
 
@@ -7,7 +10,9 @@ public class Application {
 
     public static void main(String[] args) {
 
-        SpeakerService service = new SpeakerServiceImpl();
+        ApplicationContext appContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        SpeakerService service = appContext.getBean("speakerService",SpeakerService.class);
 
         System.out.println(service.findAll().get(0).getFirstName());
 
--- src/main/java/com/tutorials/spring/services/SpeakerServiceImpl.java
+++ src/main/java/com/tutorials/spring/services/SpeakerServiceImpl.java
@@ -3,16 +3,20 @@ package com.tutorials.spring.services;
 import java.util.List;
 
 import com.tutorials.spring.models.Speaker;
-import com.tutorials.spring.repositories.HibernateSpeakerRepositoryImpl;
 import com.tutorials.spring.repositories.SpeakerRepository;
 
 public class SpeakerServiceImpl implements SpeakerService {
     
-    private SpeakerRepository repository = new HibernateSpeakerRepositoryImpl();
+    private SpeakerRepository repository ;
 
     @Override
     public List<Speaker> findAll(){
         return repository.findAll();
     }
-    
+
+    public void setSpeakerRepository(SpeakerRepository repository) {
+        System.out.println("setter called");
+        this.repository = repository;
+    }
+
 }
